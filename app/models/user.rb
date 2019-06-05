c0b774173b7e816f0ad87c0d06c88633fcf80051
root <root@vultr.guest>
2019-06-05 15:06:24 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   before_save :downcase_email
   before_create :create_activation_digest
@@ -14,6 +14,7 @@ class User < ApplicationRecord
 #   渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost=ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end  
   
 #   ランダムなトークンを返す
@@ -26,10 +27,12 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
   
+  # トークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
-    digest=send("#{attribute}_digest")
+    digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
+    #return true
   end
   
   def forget
@@ -37,11 +40,24 @@ class User < ApplicationRecord
   end
   
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
   
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+  
+  def create_reset_digest
+    self.reset_token=User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+  
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   
   
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,11 +33,20 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   host = 'twitter.natsuo720.com' 
-  config.action_mailer.default_url_options = { host: 'twitter.natsuo720.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'twitter.natsuo720.com', protocol: 'http' }
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => 'lgraph.user@gmail.com',
+    :password => 'n00286203',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+    }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
